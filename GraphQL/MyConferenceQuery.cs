0f1a16ff00b9c64ddfb9809aa60f38e355f0c9b2
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2020-06-18 15:33:01 +0200

    extend query

--- GraphQL/MyConferenceQuery.cs
+++ GraphQL/MyConferenceQuery.cs
@@ -6,10 +6,35 @@ namespace MyConference.GraphQL
 {
     public class MyConferenceQuery : ObjectGraphType
     {
-        public MyConferenceQuery(SpeakerRepository speakerRepository)
+        public MyConferenceQuery(SpeakerRepository speakerRepository, SessionsRepository sessionsRepository)
         {
-            FieldAsync<ListGraphType<SpeakerType>>("allSpeaker", 
+            FieldAsync<ListGraphType<SpeakerType>>("allSpeaker",
                 resolve: async context => await speakerRepository.AllSpeakerAsync());
+
+            FieldAsync<SpeakerType>("speaker",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id" }),
+                resolve: async context =>
+                {
+                    string id = context.GetArgument<string>("id");
+                    return await speakerRepository.SpeakerAsync(id);
+                });
+
+            FieldAsync<ListGraphType<SessionType>>("allSessions",
+                arguments: new QueryArguments(new QueryArgument<BooleanGraphType> { Name = "approvedOnly", DefaultValue = true }),
+                resolve: async context =>
+                {
+                    bool approvedOnly = context.GetArgument<bool>("approvedOnly");
+                    return await sessionsRepository.AllSessionsAsync(approvedOnly);
+                }
+                );
+
+            FieldAsync<SessionType>("session",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id" }),
+                resolve: async context =>
+                {
+                    string sessionId = context.GetArgument<string>("id");
+                    return await sessionsRepository.SessionAsync(sessionId);
+                });
         }
     }
 }
